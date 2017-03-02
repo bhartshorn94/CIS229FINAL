1c94657069dc67704176676e6eb69f69cb85452f
brenden <brendenhartshorn@gmail.com>
2017-03-02 13:41:36 -0800

    Scaffolding problem controller

---
+++ Models/Lift.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MountainOperations.Models
+{
+    public class Lift
+    {
+        public virtual Boolean Lift1 { get; set; }
+        public virtual Boolean Lift2 { get; set; }
+        public virtual Boolean Lift3 { get; set; }
+        public virtual Boolean Lift4 { get; set; }
+        public virtual Boolean Lift5 { get; set; }
+        public virtual Boolean Lift6 { get; set; }
+        public virtual Boolean Lift7 { get; set; }
+        public virtual Boolean Lift8 { get; set; }
+        public virtual Boolean Lift9 { get; set; }
+        public virtual Boolean Lift10 { get; set; }
+        public virtual Boolean Lift11 { get; set; }
+        public virtual Boolean Lift12 { get; set; }
+    }
+}
---
+++ Models/LiftDB.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace MountainOperations.Models
+{
+    public class LiftDB : DbContext
+    {
+        public LiftDB() : base("name=LiftDB")
+        {
+        }
+        public System.Data.Entity.DbSet<MountainOperations.Models.Lift> Lifts { get; set; }
+    }
+}
